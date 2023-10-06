ef85e63f341650ee1152ea7fbe87b3821f055d4b
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-06 10:25:39 +0200

    http foundation

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php // index.php
 
-require_once __DIR__ . '/model.php';
-require_once __DIR__ . '/controllers.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
 
 // route the request internally
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($uri === '/index.php' || $uri === '/') {
-    list_action();
-} elseif (($uri === '/index.php/show' || $uri == '/show') && isset($_GET['id'])) {
-    show_action($_GET['id']);
+$uri = $request->getPathInfo();
+if ($uri === '/') {
+    $response = list_action();
+} elseif ($uri == '/show' && $request->query->has('id')) {
+    $response = show_action($request->query->get('id'));
 } else {
-    http_response_code(404);
-    echo '<html><body><h1>Page Not Found</h1></body>';
+    $html = '<html><body><h1>Page Not Found</h1></body>';
+    $response = new Response($html, Response::HTTP_NOT_FOUND);
 }
+
+// echo the headers and send the response
+$response->send();
 
 // based on https://symfony.com/doc/current/introduction/from_flat_php_to_symfony.html

bf98aa974fff64c9ce7f5a93277e3eaca25c57c9
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-09 13:46:02 +0200

    first controller

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class WeatherController
+{
+    public function highlanderSays(): Response
+    {
+        // draw an integer from 0 to 100
+        $draw = random_int(0, 100);
+
+        // if the value is < 50 (%) then say it's gonna rain
+        // otherwise say it's gonna be sunny
+        $forecast = $draw < 50 ? "It's going to rain" : "It's going to be sunny";
+
+        // return response
+        return new Response(
+            "<html><body>$forecast</body></html>"
+        );
+    }
+}

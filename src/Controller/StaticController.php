c0ae877e8de0d04acdca46e214d4eea08f6c32cf
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-09 15:51:20 +0200

    static info page

---
+++ src/Controller/StaticController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticController extends AbstractController
+{
+    #[Route('/')]
+    public function home(): Response
+    {
+        return $this->render('static/home.html.twig', []);
+    }
+}

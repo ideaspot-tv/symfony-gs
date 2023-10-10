344ab0e83112d461aa5927011e887823cf97ef05
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-10 15:22:56 +0200

    inlining

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -9,8 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/highlander-says/{threshold}', requirements: ['threshold' => '\d+'])]
-    public function highlanderSays(int $threshold = 50): Response
+//    #[Route('/weather/highlander-says/{threshold<\d+>?50}')]
+    public function highlanderSays(int $threshold): Response
     {
         // draw an integer from 0 to 100
         $draw = random_int(0, 100);
@@ -25,7 +25,7 @@ class WeatherController extends AbstractController
         ]);
     }
 
-    #[Route('/weather/highlander-says/{guess}')]
+//    #[Route('/weather/highlander-says/{guess}')]
     public function highlanderSaysGuess(string $guess): Response
     {
         $forecast = "It's going to $guess";

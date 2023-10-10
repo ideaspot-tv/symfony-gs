c6c7fae2d7d7225085eb6b7e8be6efdb7b3e73bc
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-10 10:28:40 +0200

    route parameters

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -9,15 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/highlander-says', name: 'this_is_my_name', methods: ['GET', 'POST'])]
-    public function highlanderSays(): Response
+//    #[Route('/weather/highlander-says/{threshold}', name: 'this_is_my_name', methods: ['GET', 'POST'])]
+    public function highlanderSays(int $threshold): Response
     {
         // draw an integer from 0 to 100
         $draw = random_int(0, 100);
 
         // if the value is < 50 (%) then say it's gonna rain
         // otherwise say it's gonna be sunny
-        $forecast = $draw < 50 ? "It's going to rain" : "It's going to be sunny";
+        $forecast = $draw < $threshold ? "It's going to rain" : "It's going to be sunny";
 
         // return response
         return $this->render('weather/highlander_says.html.twig', [

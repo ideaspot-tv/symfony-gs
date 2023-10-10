e1722eb65def279db84db6f2bcb6c9170cbdd8b9
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-10 15:25:05 +0200

    grouping prefixing

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,9 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+//#[Route('/weather')]
 class WeatherController extends AbstractController
 {
-//    #[Route('/weather/highlander-says/{threshold<\d+>?50}')]
+//    #[Route('/highlander-says/{threshold<\d+>?50}')]
     public function highlanderSays(int $threshold): Response
     {
         // draw an integer from 0 to 100
@@ -25,7 +26,7 @@ class WeatherController extends AbstractController
         ]);
     }
 
-//    #[Route('/weather/highlander-says/{guess}')]
+//    #[Route('/highlander-says/{guess}')]
     public function highlanderSaysGuess(string $guess): Response
     {
         $forecast = "It's going to $guess";

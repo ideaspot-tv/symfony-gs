36a85fc83b14a8e4b4975f00f4a8ce1039b28d03
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-10 16:10:42 +0200

    generating urls

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
@@ -20,6 +21,11 @@ class WeatherController extends AbstractController
 
         $json = [
             'forecast' => $forecast,
+            'self' => $this->generateUrl(
+                'app_weather_highlandersaysapi',
+                ['threshold' => $threshold],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            ),
         ];
 
         return new JsonResponse($json);

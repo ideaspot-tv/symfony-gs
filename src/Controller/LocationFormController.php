1507e1d50e6799d4fa9c6df90abf542c2f1e931f
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-17 16:26:23 +0200

    form

---
+++ src/Controller/LocationFormController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/location-form')]
+class LocationFormController extends AbstractController
+{
+    #[Route('/new')]
+    public function new(): Response
+    {
+        $location = new Location();
+        $location->setCountryCode('PL');
+
+        $form = $this->createFormBuilder($location)
+            ->add('name', TextareaType::class)
+            ->add('countryCode')
+            ->add('latitude')
+            ->add('longitude')
+            ->getForm();
+
+        return $this->render('location_form/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}

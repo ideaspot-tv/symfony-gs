656ad3535560f153be1f75cc20a1f78b0cacb1f5
Artur Karczmarczyk <artur@ideaspot.pl>
2023-10-28 16:19:11 +0200

    CRUDs

---
+++ src/Form/ForecastType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Forecast;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForecastType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date')
+            ->add('temperatureCelsius')
+            ->add('flTemperatureCelsius')
+            ->add('pressure')
+            ->add('humidity')
+            ->add('windSpeed')
+            ->add('windDeg')
+            ->add('cloudiness')
+            ->add('icon')
+            ->add('location')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Forecast::class,
+        ]);
+    }
+}

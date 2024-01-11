ef19e9a47928bc54e16b35ffbababa30522e7ede
Artur Karczmarczyk <artur@ideaspot.pl>
2024-01-11 13:47:16 +0100

    PHP8 / Symfony 6.4 update

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 // read request
@@ -8,7 +10,18 @@ $request = Request::createFromGlobals();
 $uri = $request->getPathInfo();
 $foo = $request->get('foo', 'not set');
 
-// write response
-header('Content-Type: text/html');
-echo "The URI requested is: <pre>$uri</pre><br>\n";
-echo "The value of the <pre>foo</pre> parameter is: <pre>$foo</pre>";
+// prepare response
+$response = new Response();
+$response->setContent(<<<EOT
+<html>
+<body>
+The URI requested is: <pre>$uri</pre><br>
+The value of the <pre>foo</pre> parameter is: <pre>$foo</pre>
+</body>
+</html>
+EOT);
+$response->setStatusCode(Response::HTTP_OK);
+$response->headers->set('Content-Type', 'text/html');
+
+// send response
+$response->send();

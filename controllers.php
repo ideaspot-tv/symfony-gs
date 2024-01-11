3cc26efa8f131d493f7ea0fea67ef99d8c1c8716
Artur Karczmarczyk <artur@ideaspot.pl>
2024-01-11 14:29:03 +0100

    PHP8 / Symfony 6.4 update

--- controllers.php
+++ controllers.php
@@ -1,17 +1,35 @@
 <?php // controllers.php
 
+use Symfony\Component\HttpFoundation\Response;
+
 function list_action()
 {
     $posts = get_all_posts();
+    $html = render_template('templates/list.html.php', [
+        'posts' => $posts,
+    ]);
 
-    require __DIR__.'/templates/list.html.php';
+    return new Response($html);
 }
 
 function show_action($id)
 {
     $post = get_post_by_id($_REQUEST['id']);
+    $html = render_template('templates/show.html.php', [
+        'post' => $post,
+    ]);
 
-    require __DIR__.'/templates/show.html.php';
+    return new Response($html);
+}
+
+function render_template($path, array $args)
+{
+    extract($args);
+    ob_start();
+    require __DIR__ . '/' . $path;
+    $html = ob_get_clean();
+
+    return $html;
 }
 
 // based on https://symfony.com/doc/current/introduction/from_flat_php_to_symfony.html

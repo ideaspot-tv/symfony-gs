e9c95cddb3bfb9f79ab537779d47f915ad51c42c
Artur Karczmarczyk <artur@ideaspot.pl>
2024-02-06 16:15:33 +0100

    Update to SF 6.4

---
+++ public/xss.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>XSS Example</title>
+</head>
+<body>
+
+<?php if ($_REQUEST['name'] ?? null): ?>
+    <div>Hello <strong><?= $_REQUEST['name'] ?></strong>!</div>
+<?php else: ?>
+    <div>Enter your name:</div>
+<?php endif; ?>
+
+<form>
+    <input type="text" name="name" value="<?= $_REQUEST['name'] ?? '' ?>">
+</form>
+</body>
+</html>
+
+<?php
+// <script>alert('hello!')</script>

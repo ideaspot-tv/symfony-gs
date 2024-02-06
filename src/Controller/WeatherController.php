5b35c1b77ca18e36dcf452ef2a3a92fc66a2cebd
Artur Karczmarczyk <artur@ideaspot.pl>
2024-02-06 11:10:04 +0100

    Update to SF 6.4

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
+    #[Route('/highlander-says/{threshold<\d+>?50}', host: 'api.localhost')]
+    public function highlanderSaysApi(int $threshold): Response
+    {
+        $draw = random_int(0, 100);
+
+        $forecast = $draw < $threshold ? "It's going to rain" : "It's going to be sunny";
+
+        $json = [
+            'forecast' => $forecast,
+        ];
+
+        return new JsonResponse($json);
+    }
+
     #[Route('/highlander-says/{threshold<\d+>?50}')]
     public function highlanderSays(int $threshold): Response
     {

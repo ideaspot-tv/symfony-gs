98094f375d60e33db70968747236777cdb6c01a1
Artur Karczmarczyk <artur@ideaspot.pl>
2024-02-12 16:28:51 +0100

    Update to SF 6.4

---
+++ src/Form/LocationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('countryCode', ChoiceType::class, [
+                'choices'  => [
+                    '(choose)' => null,
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'Spain' => 'ES',
+                    'France' => 'FR',
+                    'United States' => 'US',
+                    'Portugal' => 'PT',
+                ],
+            ])
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}

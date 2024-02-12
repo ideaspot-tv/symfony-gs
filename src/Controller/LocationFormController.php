8983120d40e897b5d408888fb9ebdc6d46f1fdf3
Artur Karczmarczyk <artur@ideaspot.pl>
2024-02-12 15:28:00 +0100

    Update to SF 6.4

--- src/Controller/LocationFormController.php
+++ src/Controller/LocationFormController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use App\Form\LocationFormTestType;
+use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,11 +18,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class LocationFormController extends AbstractController
 {
     #[Route('/new')]
-    public function new(): Response
+    public function new(
+        Request $request,
+        LocationRepository $repository,
+    ): Response
     {
         $location = new Location();
 
         $form = $this->createForm(LocationFormTestType::class, $location);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repository->save($location, true);
+
+            return $this->redirectToRoute('app_location_index');
+        }
 
         return $this->render('location_form/new.html.twig', [
             'form' => $form,

f4ca00ac88da617315c3c5f93b44f931c46295d6
Artur Karczmarczyk <artur@ideaspot.pl>
2024-02-14 13:42:58 +0100

    Update to SF 6.4

--- src/Service/Math.php
+++ src/Service/Math.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+
 class Math
 {
+
+    public function __construct(
+        private LoggerInterface $logger
+    )
+    {
+    }
+
     public function add($a, $b)
     {
+        $this->logger->debug("a+b");
         return $a + $b;
     }
 }
